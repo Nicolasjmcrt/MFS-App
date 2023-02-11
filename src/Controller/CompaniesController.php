2d1708c446be9ee9ef65216aa6904fa7dec18fe0
Nicolasjmcrt <jumeaucourtn@gmail.com>
2023-02-11 12:35:43 +0100

    Some more templates and controllers
    Adding new templates and controllers.
    Creating Airport entity

---
+++ src/Controller/CompaniesController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Company;
+use App\Repository\CompanyRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+#[Route('/companies', name: 'companies_')]
+class CompaniesController extends AbstractController
+{
+    #[Route('/', name: 'index')]
+    public function index(CompanyRepository $companyRepository): Response
+    {
+        return $this->render('companies/index.html.twig', [
+            'companies' => $companyRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/{id}', name: 'show')]
+    public function show(Company $company): Response
+    {
+        return $this->render('companies/show.html.twig', compact('company'));
+    }
+}
